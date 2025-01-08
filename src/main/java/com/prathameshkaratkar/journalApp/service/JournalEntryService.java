9e7b79b88fbffeef5bb109b29464f5eac501579c
Prathameshkaratkar <karatkarprathamesh@gmail.com>
2025-01-08 12:06:45 +0530

    transactional

--- src/main/java/com/prathameshkaratkar/journalApp/service/JournalEntryService.java
+++ src/main/java/com/prathameshkaratkar/journalApp/service/JournalEntryService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -24,12 +25,13 @@ public class JournalEntryService {
     private UserService userService;
 
 
-
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName) {
       User user = userService.findByUserName(userName);
       journalEntry.setDate(LocalDateTime.now());
       JournalEntry saved = journalEntryRepository.save(journalEntry);
       user.getJournalEntries().add(saved);
+      user.setUserName(null);
       userService.saveEntry(user);
 
     }
